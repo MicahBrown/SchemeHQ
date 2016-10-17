3bf864bdf53696fc8e5bad39dae3ff7031351486
Micah <micahyb@gmail.com>
2016-10-17 00:23:01 -0500

    added model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :discussion
+
+  validates :user, presence: true
+  validates :discussion, presence: true
+  validates :message, presence: true
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,7 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :user, presence: true
+  validates :title, presence: true
 end
