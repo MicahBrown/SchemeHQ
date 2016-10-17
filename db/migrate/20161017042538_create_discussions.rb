4abffd086341923a26ce13af10d4e0862bac5187
Micah <micahyb@gmail.com>
2016-10-16 23:30:55 -0500

    generate discussion resource

--- db/migrate/20161017042538_create_discussions.rb
+++ db/migrate/20161017042538_create_discussions.rb
@@ -1,8 +1,10 @@
 class CreateDiscussions < ActiveRecord::Migration[5.0]
   def change
     create_table :discussions do |t|
-
-      t.timestamps
+      t.belongs_to :user, null: false
+      t.string :title,    null: false
+      t.boolean :private, null: false, default: false
+      t.timestamps        null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017031748) do
+ActiveRecord::Schema.define(version: 20161017042538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.integer  "user_id",                    null: false
+    t.string   "title",                      null: false
+    t.boolean  "private",    default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["user_id"], name: "index_discussions_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "display_name",           default: "", null: false
