1b1a82672048e580b874a6626766f728d34b0ec8
Micah <micahyb@gmail.com>
2016-10-16 23:43:29 -0500

    set up discussion creation in controller

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,9 +1,22 @@
 class DiscussionsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource param_method: :discussion_params
 
   def new
   end
 
   def create
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to @discussion, notice: "Successfully started discussion!" }
+      else
+        format.html { render :new }
+      end
+    end
   end
+
+  private
+
+    def discussion_params
+      params.require(:discussion).permit(:title)
+    end
 end
