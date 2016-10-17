8b0ddb3030cad9f1b71504d89932bd7a717e700c
Micah <micahyb@gmail.com>
2016-10-17 00:19:42 -0500

    set create action for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource :discussion
+  load_and_authorize_resource through: :discussion, param_method: :comment_params
+
+  def create
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.discussion, notice: "Successfully saved comment!" }
+      else
+        format.html { redirect_to @commetn.discussion, alert: "Unable to save comment." }
+      end
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:message)
+    end
 end
