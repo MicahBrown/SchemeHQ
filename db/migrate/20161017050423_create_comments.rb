988df254fbe21043cc1a466dcfa103333fe54868
Micah <micahyb@gmail.com>
2016-10-17 00:09:36 -0500

    created comments table

--- db/migrate/20161017050423_create_comments.rb
+++ db/migrate/20161017050423_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-
-      t.timestamps
+      t.belongs_to :user,       null: false
+      t.belongs_to :discussion, null: false
+      t.text :message,          null: false
+      t.timestamps              null: false
     end
   end
 end
