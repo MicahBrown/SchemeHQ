4ca75acd294ed089178cb602d2b9326d983e6a60
Micah <micahyb@gmail.com>
2016-10-17 23:53:21 -0500

    set up create action for polls controller

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,2 +1,20 @@
 class PollsController < ApplicationController
+  load_and_authorize_resource :discussion, find_by: :token
+  load_and_authorize_resource through: :discussion, param_method: :poll_params
+
+  def create
+    respond_to do |format|
+      if @poll.save
+        format.html { redirect_to @discussion, notice: "Successfully saved poll!" }
+      else
+        format.html { redirect_to @discussion, alert: "Unable to save poll." }
+      end
+    end
+  end
+
+  private
+
+    def poll_params
+      params.require(:poll).permit(:title, poll_options_attributes: [:value])
+    end
 end
