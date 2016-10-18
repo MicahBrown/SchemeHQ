a5308ff3178bc852a425ec5835321528a424786e
Micah <micahyb@gmail.com>
2016-10-17 22:41:49 -0500

    set default display name for new user records

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   has_many :discussions
   has_many :comments
   has_many :polls
+
+  before_validation :set_display_name
+
+  def set_display_name
+    return true if persisted? || email.blank? || display_name.present?
+
+    self.display_name = self.email
+  end
 end
