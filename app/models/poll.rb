cc9a92faf35688a06705307deea40181dc19014b
Micah <micahyb@gmail.com>
2016-10-17 23:55:24 -0500

    use nested attributes on poll options

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,6 +2,7 @@ class Poll < ApplicationRecord
   belongs_to :user
   belongs_to :discussion
   has_many :poll_options
+    accepts_nested_attributes_for :poll_options
 
   validates :title, presence: true
   validates :user, presence: true
