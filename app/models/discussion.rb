013a5cd91696350372a3bd58af106a82aa808700
Micah <micahyb@gmail.com>
2016-10-17 22:14:27 -0500

    set up model associations for new poll objects

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,7 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :polls
 
   validates :user, presence: true
   validates :title, presence: true
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,5 @@
 class Poll < ApplicationRecord
+  belongs_to :user
+  belongs_to :discussion
+  has_many :poll_options
 end
