6ac7e7a9019a3eb90e66051eda326c818204abd0
Micah <micahyb@gmail.com>
2016-10-17 22:56:42 -0500

    added model validations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,8 @@ class Poll < ApplicationRecord
   belongs_to :user
   belongs_to :discussion
   has_many :poll_options
+
+  validates :title, presence: true
+  validates :user, presence: true
+  validates :discussion, presence: true
 end
--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,3 +1,7 @@
 class PollOption < ApplicationRecord
   belongs_to :poll
+
+  validates :poll, presence: true
+  validates :value, presence: true
+  validates :position, presence: true
 end
