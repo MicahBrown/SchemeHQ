24d3a8e0bcd070723fa9c707aad92cf8788ad7d8
Micah <micahyb@gmail.com>
2016-10-19 22:33:03 -0500

    added wrapper helper for reveal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def reveal name, options={}, &block
+    css_class = options.has_key?(:class) ? "reveal #{options[:class]}" : "reveal"
+
+    content_tag :div, capture(&block), options.deep_merge( id:    name,
+                                                           class: css_class,
+                                                           data:  { reveal: ''} )
+  end
 end
