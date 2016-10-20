382624163e8c8668c327e9b7191fe08af8608f4e
Micah <micahyb@gmail.com>
2016-10-19 22:40:51 -0500

    added close button to reveal helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def reveal name, options={}, &block
     css_class = options.has_key?(:class) ? "reveal #{options[:class]}" : "reveal"
+    options   = options.deep_merge id:    name,
+                                   class: css_class,
+                                   data:  { reveal: ''}
 
-    content_tag :div, capture(&block), options.deep_merge( id:    name,
-                                                           class: css_class,
-                                                           data:  { reveal: ''} )
+    content_tag :div, capture(&block) + close_button, options
+  end
+
+  def close_button
+    button_tag '&times;'.html_safe, class: 'close-button secondary-text', data: { close: '' }
   end
 end
