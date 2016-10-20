723363835e5a21900eb36e0e072f78ff5104574e
Micah <micahyb@gmail.com>
2016-10-20 00:21:31 -0500

    added helper to conditionally wrap content in a wrapper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
   def close_button
     button_tag '&times;'.html_safe, class: 'close-button secondary-text', data: { close: '' }
   end
+
+  def wrap_content_tag_if(condition, tag, options={}, &block)
+    if condition
+      content_tag(tag, capture(&block), options)
+    else
+      capture(&block)
+    end
+  end
 end
