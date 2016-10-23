dfc980fbd3cc98f6a7054a46ddb31c467e6040c5
Micah <micahyb@gmail.com>
2016-10-23 18:07:55 -0500

    make discussion entries table

--- db/migrate/20161023230056_create_discussion_entries.rb
+++ db/migrate/20161023230056_create_discussion_entries.rb
@@ -1,8 +1,10 @@
 class CreateDiscussionEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :discussion_entries do |t|
-
-      t.timestamps
+      t.belongs_to :discussion, null: false
+      t.references :discussable, polymorphic: true, index: true
+      t.integer :position, default: 0, null: false
+      t.timestamps null: false
     end
   end
 end
