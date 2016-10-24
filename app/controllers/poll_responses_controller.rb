ff9f6ad71447ce2cd1c958518353f6158bf5917a
Micah <micahyb@gmail.com>
2016-10-23 22:19:17 -0500

    added create action to poll response controller

--- app/controllers/poll_responses_controller.rb
+++ app/controllers/poll_responses_controller.rb
@@ -1,2 +1,23 @@
 class PollResponsesController < ApplicationController
+  load_and_authorize_resource :discussion, find_by: :token
+  load_and_authorize_resource :poll, through: :discussion
+  load_resource through: :poll, param_method: :poll_response_params
+
+  def create
+    @poll_response.user = current_user
+
+    respond_to do |format|
+      if @poll_response.save
+        format.html { redirect_to @discussion, notice: "Successfully saved vote!" }
+      else
+        format.html { redirect_to @discussion, alert: "Unable to save vote." }
+      end
+    end
+  end
+
+  private
+
+    def poll_response_params
+      params.require(:poll_response).permit(:poll_option_id)
+    end
 end
