c957f13297191d37f146f674c4418b453d4c5340
Micah <micahyb@gmail.com>
2016-10-26 00:42:11 -0500

    reverse discussion entries order

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,5 +1,5 @@
 module DiscussionsHelper
   def discussion_entries discussion
-    discussion.discussion_entries.includes(:discussable => :user).map(&:discussable)
+    discussion.discussion_entries.order('id DESC').includes(:discussable => :user).map(&:discussable)
   end
 end
