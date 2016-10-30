43778c11bf53d1b4fa029a4399dd7c1c5620083a
Micah <micahyb@gmail.com>
2016-10-30 18:29:45 -0500

    added timestamp to discussion entries

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -2,4 +2,11 @@ module DiscussionsHelper
   def discussion_entries discussion
     discussion.discussion_entries.order('id DESC').includes(:discussable => :user)
   end
+
+  def discussion_entry_timestamp discussion
+    value = time_ago_in_words(discussion.updated_at) + " ago"
+    value = value.gsub "about ", ""
+
+    content_tag :time, value, title: discussion.updated_at.strftime("%b %-e, %Y %-I:%M %p %Z"), data: { tooltip: true }
+  end
 end
