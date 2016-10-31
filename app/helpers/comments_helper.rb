5d212cc0a7f3046cf28769e9058f0cbef5630e33
Micah <micahyb@gmail.com>
2016-10-30 19:31:52 -0500

    added confirmation to comment delete link

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -5,7 +5,7 @@ module CommentsHelper
 
     if current_user == comment.user
       links.push link_to("Edit", discussion_comment_path(comment.discussion, comment))
-      links.push link_to("Delete", discussion_comment_path(comment.discussion, comment), method: :delete)
+      links.push link_to("Delete", discussion_comment_path(comment.discussion, comment), method: :delete, data: { confirm: "Are you sure?" })
     end
 
     action_links links
