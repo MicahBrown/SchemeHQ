f0090574a95d888f2e154237b103948195713d0f
Micah <micahyb@gmail.com>
2016-11-03 00:04:28 -0500

    added controller for responding to omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
     resources :comments, only: [:create, :edit, :update, :destroy]
   end
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
