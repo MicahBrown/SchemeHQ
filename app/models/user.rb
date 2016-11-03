b15dbdcaa8ab3bc4db8406ffaaf708a6d6c830a9
Micah <micahyb@gmail.com>
2016-11-03 00:03:12 -0500

    added method to find or initialize user with omniauth creds

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,13 @@ class User < ApplicationRecord
 
     self.display_name = self.email
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
