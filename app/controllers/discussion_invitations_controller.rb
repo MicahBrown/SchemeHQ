e19a05a955db0c2510b4b56fb210dedf4e528836
Micah <micahyb@gmail.com>
2016-11-06 02:38:25 -0600

    added create action to discussion invitations

--- app/controllers/discussion_invitations_controller.rb
+++ app/controllers/discussion_invitations_controller.rb
@@ -1,2 +1,17 @@
 class DiscussionInvitationsController < ApplicationController
+  load_and_authorize_resource :discussion, find_by: :token
+
+  def create
+    @discussion.invite(discussion_invitation_params[:invitees])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+    def discussion_invitation_params
+      params.require(:discussions).permit(invitees: [])
+    end
 end
