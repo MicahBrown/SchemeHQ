c2f5e60547b3fdac3cd448406c8d4126b8f301f4
Micah <micahyb@gmail.com>
2016-11-06 00:17:12 -0500

    link discussion facilitator after creation

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,17 +4,30 @@ class Discussion < ApplicationRecord
   has_many :polls
   has_many :discussion_participants
   has_many :discussion_entries
+  has_one  :facilitator, -> { facilitator }, class_name: "DiscussionParticipant" # condition uses the enum scope
 
   validates :user, presence: true
   validates :title, presence: true
 
   before_create :generate_token
+  after_create :assign_facilitator!
 
   def generate_token
     self[:token] = loop do
       token = SecureRandom.urlsafe_base64(8)
 
       break token unless self.class.exists?(token: token)
+    end
+  end
+
+  def assign_facilitator! new_facilitator = user
+    return unless new_facilitator
+
+    if f = facilitator
+      f.user = new_facilitator
+      f.save!
+    else
+      create_facilitator! user: new_facilitator
     end
   end
 
