178925adc33931f5ce5b2b2ee0fd85cdfd846e93
Micah <micahyb@gmail.com>
2016-11-08 21:32:08 -0600

    require email presence

--- app/models/discussion_invitation.rb
+++ app/models/discussion_invitation.rb
@@ -2,5 +2,5 @@ class DiscussionInvitation < ApplicationRecord
   belongs_to :discussion
   belongs_to :user
 
-  validates :email, uniqueness: { scope: :discussion_id }
+  validates :email, presence: true, uniqueness: { scope: :discussion_id }
 end
