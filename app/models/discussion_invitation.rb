80e9d4778842da192b42940b475fa74727bc5b8b
Micah <micahyb@gmail.com>
2016-11-09 00:33:46 -0600

    strip whitespace on discussion invitation email

--- app/models/discussion_invitation.rb
+++ app/models/discussion_invitation.rb
@@ -3,4 +3,12 @@ class DiscussionInvitation < ApplicationRecord
   belongs_to :user
 
   validates :email, presence: true, uniqueness: { scope: :discussion_id }
+
+  before_validation { strip_whitespace :email }
+
+  def strip_whitespace attr_sym
+    value = send(attr_sym)
+    value = value.strip if value.is_a?(String)
+    value
+  end
 end
