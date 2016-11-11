a27176b24dd1613d7233be187b6eeddafc7a57c3
Micah <micahyb@gmail.com>
2016-11-10 23:07:18 -0600

    validate email format

--- app/models/discussion_invitation.rb
+++ app/models/discussion_invitation.rb
@@ -2,7 +2,10 @@ class DiscussionInvitation < ApplicationRecord
   belongs_to :discussion
   belongs_to :user
 
-  validates :email, presence: true, uniqueness: { scope: :discussion_id }
+  validates :email, presence:   true,
+                    uniqueness: { scope: :discussion_id },
+                    format:     { with: EMAIL_REGEX, allow_blank: true }
+
 
   before_validation { strip_whitespace :email }
 
