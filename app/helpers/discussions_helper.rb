1005b2f400e2928414888dcdfe5df01a1662f7c3
Micah <micahyb@gmail.com>
2016-11-14 20:16:39 -0600

    put discussable author into helper

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -10,7 +10,16 @@ module DiscussionsHelper
     content_tag :time, value, title: discussion.updated_at.strftime("%b %-e, %Y %-I:%M %p %Z"), data: { tooltip: true }
   end
 
+  def discussable_author discussable
+    icon_name = case discussable
+                when Comment then 'comment'
+                when Poll    then 'bar-chart'
+                end
+
+    icon(icon_name, discussable.user.display_name, class: 'fa-fw')
+  end
+
   def clonable_discussion_invitation
-     DiscussionInvitation.new(email: "{ email }")
-   end
+    DiscussionInvitation.new(email: "{ email }")
+  end
 end
