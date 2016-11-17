d87a027d95cccce077fed5ae1c9ebc378870dd75
Micah <micahyb@gmail.com>
2016-11-16 19:04:05 -0600

    added form for updating user settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,5 +3,18 @@ class SettingsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if current_user.update_attributes(user_params)
+        format.html { redirect_to settings_path, notice: "Successfully updated settings!" }
+      else
+        format.html { render :edit }
+      end
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:display_name)
+    end
 end
