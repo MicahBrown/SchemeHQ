9afabfdf3e069daddab9b424c9ed18d9b096a258
Micah <micahyb@gmail.com>
2016-11-17 01:32:23 -0600

    added nickname creating and updating (controller)

--- app/controllers/nicknames_controller.rb
+++ app/controllers/nicknames_controller.rb
@@ -1,4 +1,28 @@
 class NicknamesController < ApplicationController
+  load_resource :user, find_by: :public_token
+  load_resource param_method: :nickname_params
+
   def create
+    @nickname.namee = @user
+    @nickname.namer = current_user
+    @nickname.save
+
+    respond_to do |format|
+      format.js
+    end
   end
+
+  def update
+    @nickname.update_attributes(nickname_params)
+
+    respond_to do |format|
+      format.js { render :create }
+    end
+  end
+
+  private
+
+    def nickname_params
+      params.require(:nickname).permit(:value)
+    end
 end
