b328c5509d7f43aacaf46dcab420d71234bec124
Micah <micahyb@gmail.com>
2016-11-23 20:02:18 -0600

    fill in user tokens prior to adding unique index

--- db/migrate/20161117052519_add_public_token_to_users.rb
+++ db/migrate/20161117052519_add_public_token_to_users.rb
@@ -1,6 +1,13 @@
 class AddPublicTokenToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :public_token, :string, null: false, default: ""
+
+    User.reset_column_information
+    User.all.each do |user|
+      user.regenerate_public_token
+      user.save
+    end
+
     add_index :users, :public_token, unique: true
   end
 
