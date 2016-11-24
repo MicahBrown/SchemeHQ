8cc7f1cb47cb567ba54a64f2f9a8c697de84704c
Micah <micahyb@gmail.com>
2016-11-24 03:25:51 -0600

    move invitation response form into a helper

--- app/helpers/schemes_helper.rb
+++ app/helpers/schemes_helper.rb
@@ -38,4 +38,14 @@ module SchemesHelper
 
     content_tag :span, status, class: css_class
   end
+
+  def invitation_response_form invite, response_value=true
+    button_to scheme_invitation_response_path(invite.scheme, invite),
+      method:     :put,
+      form_class: "invitation-response-form",
+      params:     { response: response_value },
+      data:       { disable_with: "Please wait..." } do
+      response_value ? icon("check-circle-o", "Accept") : icon("times-circle-o", "Decline")
+    end
+  end
 end
