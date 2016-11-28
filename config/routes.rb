c1fd49396863459b9c514056fefe6f86af382da2
Micah <micahyb@gmail.com>
2016-11-27 21:35:41 -0600

    added destroy action route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
 
     resources :entries do
-      resource :votes, controller: :scheme_entry_votes, only: [:create, :update]
+      resource :votes, controller: :scheme_entry_votes, only: [:create, :update, :destroy]
     end
 
     resources :polls, only: [:create, :destroy] do
