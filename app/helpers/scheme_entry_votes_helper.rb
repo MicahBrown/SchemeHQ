367a4a9bf6be7bbc03810423b720a75e5f8c909b
Micah <micahyb@gmail.com>
2016-11-27 23:46:38 -0600

    reload association after deleting record

--- app/helpers/scheme_entry_votes_helper.rb
+++ app/helpers/scheme_entry_votes_helper.rb
@@ -3,7 +3,7 @@ module SchemeEntryVotesHelper
     icon_name   = value.positive? ? "thumbs-o-up" : "thumbs-o-down"
     css_classes = value.positive? ? "upvote"      : "downvote"
     http_method = existing_vote   ? :patch        : :post
-    vote_count  = entry.scheme_entry_votes.select {|vote| vote.value == value }.size
+    vote_count  = entry.scheme_entry_votes(true).select {|vote| vote.value == value }.size
 
     if existing_vote && existing_vote.value == value
       css_classes += " voted"
