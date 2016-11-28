50f310bd0fe92b5a99c8365c9236643535aae013
Micah <micahyb@gmail.com>
2016-11-27 23:42:24 -0600

    eager load scheme entry votes

--- app/helpers/schemes_helper.rb
+++ app/helpers/schemes_helper.rb
@@ -1,6 +1,6 @@
 module SchemesHelper
   def scheme_entries scheme
-    scheme.scheme_entries.order('id DESC').includes(:schemable => :user)
+    scheme.scheme_entries.order('id DESC').includes(:scheme_entry_votes, :schemable => :user)
   end
 
   def schemable_author schemable
