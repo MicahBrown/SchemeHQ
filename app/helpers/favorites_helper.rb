501d87935b52945bb18f1161e23d822d97bc414c
Micah <micahyb@gmail.com>
2016-11-30 23:00:46 -0600

    give class to form

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -4,8 +4,9 @@ module FavoritesHelper
     fave_count    = scheme_entry.favorites(reload_faves).size
 
     button_to scheme_entry_favorites_path(scheme_entry.scheme, scheme_entry),
-      method: existing_fave ? :delete : :post,
-      class:  "favorite#{' favorited' if existing_fave}" do
+      method:     existing_fave ? :delete : :post,
+      form_class: "favorite-form",
+      class:      "favorite#{' favorited' if existing_fave}" do
       icon("heart", fave_count)
     end
   end
