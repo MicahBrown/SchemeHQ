c68e6c0c7bc39bf404fa442cfadf598a293a5bff
Micah <micahyb@gmail.com>
2016-11-30 18:08:05 -0600

    set associations for favorite model

--- app/models/scheme_entry.rb
+++ app/models/scheme_entry.rb
@@ -2,4 +2,5 @@ class SchemeEntry < ApplicationRecord
   belongs_to :scheme
   belongs_to :schemable, polymorphic: true
   has_many :scheme_entry_votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
